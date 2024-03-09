a82e718f68853903cd8fd52f4214b301b3ff7a4c
vyck22v7k527 <VyCK22V7K527@vlvh.ctu.edu.vn>
2024-03-09 14:33:24 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ApiError = require("./app/api-error");
 const contactsRouter = require("./app/routes/contact.route");
 const cors = require("cors");
 
@@ -12,4 +13,18 @@ app.get("/", (req, res)=>{
 
 });
 app.use("/api/contacts", contactsRouter);
+// handle 404 response
+app.use((req, res, next) => {
+// Code ở đây sẽ chạy khi không có route được định nghĩa nào
+// khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+return next(new ApiError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res) => {
+// Middleware xử lý lỗi tập trung.
+// Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xửlý lỗi này
+return res.status(Error.statusCode || 500).json({
+message: Error.message || "Internal Server Error",
+});
+});
 module.exports = app;
